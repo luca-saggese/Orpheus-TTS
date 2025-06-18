321031b00affab51e567dbc00eedaa0634efcf8c
Luca Saggese <luca.saggese@gmail.com>
2025-06-18 11:30:34 +0200

    wip

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t sana .
+docker build -t orpheus .
 echo "✅ Build completata!"
 echo "👉 Per eseguire il container usa:"
-echo "docker run --rm -it --gpus all -p 8083:8080 --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v /data/huggingface:/huggingface sana"
+echo "docker run --rm -it --gpus all -p 8083:8080 --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v /data/huggingface:/huggingface orpheus bash"
